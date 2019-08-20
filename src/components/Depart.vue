f51adc44a366a6988f833caff7b83d75ecfe265e
OToNaShiAKi <1362446747@qq.com>
2019-08-20 16:58:37 +0800

    Pre

---
+++ src/components/Depart.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-flex class="depart" xs12>
+    <h4 class="title">招新群</h4>
+    <v-hover v-slot:default="{ hover }" v-for="(v, i) in depart" :key="i">
+      <v-img :src="v.src" width="210" class="img">
+        <v-expand-transition>
+          <v-flex class="d-flex justify-center flex-column hover" v-if="hover">
+            <h4 class="mb-5">{{v.name}}</h4>
+            <p class="body-2">群号：{{v.qq}}</p>
+            <p class="body-2">部长：{{v.minister}}</p>
+            <p class="body-2">部长QQ：{{v.mqq}}</p>
+            <a download :href="v.src" class="caption white--text">点击下载二维码</a>
+          </v-flex>
+        </v-expand-transition>
+      </v-img>
+    </v-hover>
+  </v-flex>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "Depart",
+  computed: {
+    ...mapState(["depart"])
+  }
+};
+</script>
+
+<style scoped>
+.depart {
+  background-color: white;
+  color: #333;
+  border-top-left-radius: 42px;
+  border-top-right-radius: 42px;
+  padding: 15px 0;
+}
+.img {
+  margin: 10px auto;
+}
+.hover {
+  background-color: #1976d29f;
+  height: 100%;
+  color: white;
+}
+</style>
---
+++ src/views/Pre.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container class="top">
+    <v-layout text-center wrap>
+      <v-flex>
+        <v-img :src="logo" width="210" class="img" />
+        <h2 class="headline mb-3">科学与技术协会</h2>
+        <p class="subheading">
+          宣传、报名尚未开启
+          <br />请于报名期间扫码报名
+          <br />可加入部门招新群提前了解详情
+        </p>
+      </v-flex>
+      <depart />
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import Depart from "@/components/Depart";
+import logo from "@/assets/logo.png";
+
+export default {
+  components: {
+    Depart
+  },
+  data: () => ({
+    logo
+  })
+};
+</script>
+
+<style scoped>
+.top {
+  background-color: #1976d2;
+  color: white;
+  padding: 0;
+}
+.img {
+  margin: 20px auto;
+}
+</style>
