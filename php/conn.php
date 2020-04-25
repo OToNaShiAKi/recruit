c1ccff655644138da83b022dfd63307e6082b9c5
濯墨 <33158720+OToNaShiAKi@users.noreply.github.com>
2020-04-25 06:41:53 +0000

    Delete conn.php

--- php/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "hustmathskexie";
-$dbname = "hustmaths";
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error)
-  die(0);
